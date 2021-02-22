ed4be7c0bd39800f25eeaa9ac109069af0e06d3b
jyotichaudhary49980 <74738197+jyotichaudhary49980@users.noreply.github.com>
2021-02-22 14:47:02 +0530

    nifty500

--- first_project/Nifty500.py
+++ first_project/Nifty500.py
@@ -8,32 +8,54 @@ import requests
 from bs4 import BeautifulSoup
 
 def shownifty500(request):
-    url = requests.get(
-        'https://www.moneycontrol.com/stocks/marketstats/indexcomp.php?optex=NSE&opttopic=indexcomp&index=7')
+    url = requests.get('https://www.moneycontrol.com/stocks/marketstats/indexcomp.php?optex=NSE&opttopic=indexcomp&index=7')
     soup = bs4.BeautifulSoup(url.text, features="html.parser")
     tickers = []
     i = 0
     pricel=[]
     chngl=[]
     pchngl=[]
+    min=0
+    max=200
     y = ['ADANIPORTS.NS', 'ASIANPAINT.NS', 'AXISBANK.NS', 'BAJAJ-AUTO.NS', 'BAJAJFINSV.NS', 'BAJFINANCE.NS', 'BHARTIARTL.NS', 'BPCL.NS', 'BRITANNIA.NS', 'CIPLA.NS', 'COALINDIA.NS', 'DIVISLAB.NS', 'DRREDDY.NS', 'EICHERMOT.NS', 'GAIL.NS', 'GRASIM.NS', 'HCLTECH.NS', 'HDFC.NS', 'HDFCBANK.NS', 'HDFCLIFE.NS', 'HEROMOTOCO.NS', 'HINDALCO.NS', 'HINDUNILVR.NS', 'ICICIBANK.NS', 'INDUSINDBK.NS', 'INFY.NS', 'IOC.NS', 'ITC.NS', 'JSWSTEEL.NS', 'KOTAKBANK.NS', 'LT.NS', 'M&M.NS', 'MARUTI.NS', 'NESTLEIND.NS', 'NTPC.NS', 'ONGC.NS', 'POWERGRID.NS', 'RELIANCE.NS', 'SBILIFE.NS', 'SBIN.NS', 'SHREECEM.NS', 'SUNPHARMA.NS', 'TATAMOTORS.NS', 'TATASTEEL.NS', 'TCS.NS', 'TECHM.NS', 'TITAN.NS', 'ULTRACEMCO.NS', 'UPL.NS', 'WIPRO.NS']
-    while (i < 200):
-    	price = soup.find_all("td", {'align': 'right'})[i].text
-    	price=price.replace(',','')
-    	chng = soup.find_all("td", {'align': 'right'})[i + 1].text
-    	pchng = soup.find_all("td", {'align': 'right'})[i + 2].text
-    	pricel.append(price)
-    	chngl.append(chng)
-    	pchngl.append(pchng)
-    	i += 4
-    while (i >= 200 and i<400):
-    	price = soup.find_all("td", {'align': 'right'})[i].text
-    	price=price.replace(',','')
-    	chng = soup.find_all("td", {'align': 'right'})[i + 1].text
-    	pchng = soup.find_all("td", {'align': 'right'})[i + 2].text
-    	pricel.append(price)
-    	chngl.append(chng)
-    	pchngl.append(pchng)
-    	i += 4
+    while(max<2000):
+      while(i>=min and i<max):
+        price = soup.find_all("td", {'align': 'right'})[i].text
+        price=price.replace(',','')
+        chng = soup.find_all("td", {'align': 'right'})[i + 1].text
+        pchng = soup.find_all("td", {'align': 'right'})[i + 2].text
+        pricel.append(price)
+        chngl.append(chng)
+        pchngl.append(pchng)
+        i += 4
+      min+=200
+      max+=200
+    # while(i>=200 and i<400):
+    #     price = soup.find_all("td", {'align':'right'})[i].text
+    #     price = price.replace(',','')
+    #     chng = soup.find_all("td", {'align':'right'})[i + 1].text
+    #     pchng = soup.find_all("td", {'align':'right'})[i + 2].text
+    #     pricel.append(price)
+    #     chngl.append(chng)
+    #     pchngl.append(pchng)
+    #     i += 4
+    # while(i>=400 and i<600):
+    #     price = soup.find_all("td", {'align':'right'})[i].text
+    #     price = price.replace(',','')
+    #     chng = soup.find_all("td", {'align':'right'})[i + 1].text
+    #     pchng = soup.find_all("td", {'align':'right'})[i + 2].text
+    #     pricel.append(price)
+    #     chngl.append(chng)
+    #     pchngl.append(pchng)
+    #     i += 4
+    # while (i >= 600 and i < 800):
+    #     price = soup.find_all("td", {'align':'right'})[i].text
+    #     price = price.replace(',','')
+    #     chng = soup.find_all("td", {'align':'right'})[i + 1].text
+    #     pchng = soup.find_all("td", {'align':'right'})[i + 2].text
+    #     pricel.append(price)
+    #     chngl.append(chng)
+    #     pchngl.append(pchng)
+    #     i += 4
     data = {"price":pricel, "chng": chngl, "pchng":pchngl}
     return HttpResponse(json.dumps(data))
